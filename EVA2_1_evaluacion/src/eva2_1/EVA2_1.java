4e0b0a9ebe7edc23f2395470df99202e60a692f2
Yisu007 <L24550246@chihuahua2.tecnm.mx>
2024-10-02 17:48:52 -0600

    Add files via upload

---
+++ EVA2_1_evaluacion/src/eva2_1/EVA2_1.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_1;
+
+/**
+ *
+ * @author Xxj53
+ */
+public class EVA2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       //CICLO FOR -->CUANDO SABEMOS CUANTAS VECES SE REPITE LA INSTRUCCION 
+       //DUNCIONA COMO UN CONTADOR:INICIO, FIN(CONDICION), Y SALTO DE CONTEO 
+       //i-->INDEX-->INDICE 
+       //VAMOS A REPETIR 10 VECES CONTANDO DEL 1 AL 10
+       
+       for(int i = 0; i <=  15; i++){
+           System.out.println(i+ "-");
+           //nosostros establecemos el punto de inicio i=0 y de fin i<=15
+           //operadores de acumulacion 
+           //++ --> 1 a la variable 
+           //-- -->restar 1
+           //+= -->SUMAR "N" A LA VARIABLE 
+           
+                      }
+
+           for(int i = 10; i >= 1; i--){
+           System.out.println(i+ "-");
+           }
+       
+    }
+    
+}
+           
+
