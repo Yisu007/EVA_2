1656c87f061c9ee877dd51b5304eb628f19725ca
Yisu007 <L24550246@chihuahua2.tecnm.mx>
2024-10-10 17:49:23 -0600

    Add files via upload

---
+++ eva2_practica1/src/eva2_practica1/Eva2_practica1.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Xxj53
+ */
+public class Eva2_practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num;
+        String hola;
+        Scanner captu= new Scanner(System.in);    
+        System.out.println("introduce el numero de veces que se repita");
+        num= captu.nextInt();
+        captu.nextLine();
+       
+        
+        
+        
+       
+        for(int i=1;i<= num ;i++ ){
+         System.out.println("hola");
+         
+         
+        }
+        
+        
+        
+    }
+    
+}
