a13e16fbd01668efd14a6a11d9279ac7d771536e
Yisu007 <L24550246@chihuahua2.tecnm.mx>
2024-10-10 17:54:02 -0600

    Add files via upload

---
+++ eva2_pratica3/src/eva2_pratica3/Eva2_pratica3.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_pratica3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Xxj53
+ */
+public class Eva2_pratica3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String acum;
+        
+        Scanner captu= new Scanner(System.in);
+        System.out.println("introduce la cadena ");
+        acum=captu.nextLine();
+        for(int i= 0; i< acum.length();i++){
+            char caracter ;
+            
+                    if
+                            System.out.println(acum.charAt(i));
+                    
+        }
+    
+}
+}
---
+++ eva2_pratica3/src/eva2_pratica3/cade.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva2_pratica3;
+
+/**
+ *
+ * @author Xxj53
+ */
+class cade {
+    
+}
