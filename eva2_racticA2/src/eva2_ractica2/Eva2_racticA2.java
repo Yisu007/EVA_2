aadea3fbfffaf434ad79f1559975287ffe43040e
Yisu007 <L24550246@chihuahua2.tecnm.mx>
2024-10-10 17:48:41 -0600

    Add files via upload

---
+++ eva2_racticA2/src/eva2_ractica2/Eva2_racticA2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_ractica2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Xxj53
+ */
+public class Eva2_racticA2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String code;
+        Scanner captu=new Scanner(System.in);
+        
+    }
+    
+}
