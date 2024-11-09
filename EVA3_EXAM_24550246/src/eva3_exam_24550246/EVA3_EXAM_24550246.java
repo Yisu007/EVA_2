b852821c2bcba5d21bfc276f169c29ebae110eae
Yisu007 <L24550246@chihuahua2.tecnm.mx>
2024-11-08 18:23:24 -0600

    Add files via upload

---
+++ EVA3_EXAM_24550246/src/eva3_exam_24550246/EVA3_EXAM_24550246.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_exam_24550246;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Xxj53
+ */
+public class EVA3_EXAM_24550246 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int piedra=1,papel=2,tijera=3;
+        
+        Scanner captu= new Scanner (System.in);
+        Random captuscaner  = new Random();
+        
+     System.out.println("elije  piedra=1,papel=2  o tijera=3 ");
+     System.out.println("  ");
+     captu.nextLine();
+     System.out.println(Math.random());
+     Math.random();
+   
+    
+
+    
+     
+    
+  
+      
+        
+    }
+    
+}
